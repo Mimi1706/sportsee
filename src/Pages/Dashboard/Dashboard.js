68ffec5c7a521122c06d3453268ab01c88a1cf4a
Mimi <hana.nguyen@outlook.fr>
2022-04-13 17:35:45 +0200

    Ajout de Header et SideBar

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import "./Dashboard.css"
+
+import Header from "../../Components/Header/Header"
+import SideBar from "../../Components/SideBar/SideBar"
+
+const Dashboard = () => {
+  return (
+    <main>
+      <Header />
+      <SideBar />
+    </main>
+  )
+}
+
+export default Dashboard
