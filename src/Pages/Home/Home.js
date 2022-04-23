f700befd52b4e9f3af64cd216b56ed5ffb9f44a8
Mimi <hana.nguyen@outlook.fr>
2022-04-23 17:39:25 +0200

    Création page de sélection utilisateur/API

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,24 @@
 import "./Home.css"
 
+import { Link } from "react-router-dom"
+
+import Logo from "../../Assets/Logo.png"
+
 const Home = () => {
-  return <main className="Home">HomePage</main>
+  return (
+    <main className="Home">
+      <img src={Logo} className="headerLogo" alt="Logo Sportsee"></img>
+      <div>
+        <Link to={`/dashboard/12`}>
+          <button>Utilisateur 12</button>
+        </Link>
+        <Link to={`/dashboard/18`}>
+          <button>Utilisateur 18</button>
+        </Link>
+      </div>
+      <button>API</button>
+    </main>
+  )
 }
 
 export default Home
