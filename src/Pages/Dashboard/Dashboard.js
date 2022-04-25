d4d60c0a9df370cfa546a4e52b65298a0dbeccdd
Mimi <hana.nguyen@outlook.fr>
2022-04-26 00:45:30 +0200

    Ajout du switch API/Mock

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -15,57 +15,72 @@ import ChartRadar from "../../Components/Charts/ChartRadar"
 import ChartPie from "../../Components/Charts/ChartPie"
 
 import urldatamock from "../../Utils/urldatamock"
-import apifetch from "../../Utils/Apifetch"
+import urldataAPI from "../../Utils/urldataAPI"
+import Apifetch from "../../Utils/Apifetch"
 import { useParams } from "react-router-dom"
+import { useContext } from "react"
+import { fetchContext } from "../../Context/fetchContext"
 
 const Dashboard = () => {
   let { idURL } = useParams()
+  let loadingComplete = true
 
-  const dataMain = apifetch(urldatamock.urlMainData(idURL))
-  const activity = apifetch(urldatamock.urlActivity(idURL))
-  const sessions = apifetch(urldatamock.urlSessions(idURL))
-  const performance = apifetch(urldatamock.urlPerformance(idURL))
+  // Switches the fetch from API to Mock
+  const { fetch } = useContext(fetchContext)
+  const url = fetch === "Mock" ? urldatamock : urldataAPI
 
-  return dataMain === null ||
+  const mainData = Apifetch(url.urlMainData(idURL))
+  const activity = Apifetch(url.urlActivity(idURL))
+  const sessions = Apifetch(url.urlSessions(idURL))
+  const performance = Apifetch(url.urlPerformance(idURL))
+
+  // Checks if all necessary data has been loaded
+  if (
+    mainData === null ||
     activity === null ||
     sessions === null ||
-    performance === null ? (
-    console.log("Chargement...")
+    performance === null
+  ) {
+    return (loadingComplete = false)
+  }
+
+  return loadingComplete === false ? (
+    alert("Chargement...")
   ) : (
     <>
       <Header />
       <main className="Dashboard">
         <h1>
-          Bonjour <span>{dataMain.data.userInfos.firstName}</span>
+          Bonjour <span>{mainData.userInfos.firstName}</span>
         </h1>
         <h2>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
         <section className="Activities">
           <div className="ChartsWrapper">
-            <ChartBar data={activity.data.sessions} />
-            <ChartLine data={sessions.data.sessions} />
+            <ChartBar data={activity.sessions} />
+            <ChartLine data={sessions.sessions} />
             <ChartRadar data={performance.data} />
-            <ChartPie data={dataMain.data.score} />
+            <ChartPie data={mainData.score} />
           </div>
           <div className="KeyDataWrapper">
             <KeyData
               src={caloriesIcon}
               name="Calories"
-              number={dataMain.data.keyData.calorieCount + "kCal"}
+              number={mainData.keyData.calorieCount + "kCal"}
             />
             <KeyData
               src={proteinIcon}
               name="Proteines"
-              number={dataMain.data.keyData.proteinCount + "g"}
+              number={mainData.keyData.proteinCount + "g"}
             />
             <KeyData
               src={carbsIcon}
               name="Glucides"
-              number={dataMain.data.keyData.carbohydrateCount + "g"}
+              number={mainData.keyData.carbohydrateCount + "g"}
             />
             <KeyData
               src={fatIcon}
               name="Lipides"
-              number={dataMain.data.keyData.lipidCount + "g"}
+              number={mainData.keyData.lipidCount + "g"}
             />
           </div>
         </section>
