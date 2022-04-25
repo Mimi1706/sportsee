ebf3a2097206007df1d61ea0c6fda6fbdee22d72
Mimi <hana.nguyen@outlook.fr>
2022-04-26 00:43:29 +0200

    Ajout redirection vers page Error

--- src/Utils/Apifetch.js
+++ src/Utils/Apifetch.js
@@ -1,21 +1,29 @@
 import { useEffect, useState } from "react"
+import { Navigate, useNavigate } from "react-router-dom"
+import ConvertData from "../Mapper/ConvertData"
 
 /**
- * Send custom request using fetch api
+ * Send a request using fetch api
  * @param { String } url
  * @return { JSON }
  */
 
 const Apifetch = (url) => {
   const [data, setData] = useState(null)
+  const Navigate = useNavigate()
 
   useEffect(() => {
     fetch(url)
       .then((resp) => resp.json())
       .then((resp) => {
-        setData(resp)
+        setData(ConvertData(resp, url))
       })
-  }, [url])
+      .catch((error) => {
+        if (error) {
+          Navigate("/error")
+        }
+      })
+  }, [url, Navigate])
 
   return data
 }
