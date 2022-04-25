c66d64bc945caa4269a3ad19e76f1e86b356c14e
Mimi <hana.nguyen@outlook.fr>
2022-04-26 00:40:38 +0200

    Création d'une page d'erreur

---
+++ src/Pages/Error/Error.js
@@ -0,0 +1,17 @@
+import "./Error.css"
+import { Link } from "react-router-dom"
+import Logo from "../../Assets/Logo.png"
+
+const Error = () => {
+  return (
+    <main className="Error">
+      <img src={Logo} className="headerLogo" alt="Logo Sportsee"></img>
+      <p>Oups... On dirait qu'il y a eu une erreur !</p>
+      <Link to={`/`}>
+        <button>Retourner vers l'accueil</button>
+      </Link>
+    </main>
+  )
+}
+
+export default Error
