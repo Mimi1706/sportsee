14c458c34a595390c113fe73c75c325309fedc65
Mimi <hana.nguyen@outlook.fr>
2022-04-26 00:43:02 +0200

    Ajout du chemin d'erreur

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { FetchProvider } from "./Context/fetchContext"
 
 import Dashboard from "./Pages/Dashboard/Dashboard"
 import Home from "./Pages/Home/Home"
+import Error from "./Pages/Error/Error"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<Home />}></Route>
-          <Route path="/dashboard/:idURL" element={<Dashboard />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <FetchProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<Home />}></Route>
+            <Route path="/dashboard/:idURL" element={<Dashboard />}></Route>
+            <Route path="/error" element={<Error />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </FetchProvider>
     </div>
   )
 }
